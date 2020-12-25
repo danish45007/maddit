bce9123938bdc06b18ffb85863b7667b8818c564
danish45007 <danish45007@gmail.com>
2020-12-26 02:12:13 +0530

    register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,18 +1,81 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Link from "next/Link";
 
 export default function Register() {
   return (
     <div className="flex">
       <Head>
-        <title>Maddit: Register</title>
+        <title>Maddit:Register</title>
         <link rel="icon" href="/reddit.svg" />
       </Head>
 
       <div
-        className="w-40 h-screen"
+        className="w-40 h-screen bg-center bg-cover"
         style={{ backgroundImage: "url('/images/bricks.jpg')" }}
       ></div>
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-72">
+          <h1 className="mb-2 text-lg font-medium">Sign up</h1>
+          <p className="mb-10 text-xs">
+            By continuing, you agree to our{" "}
+            <a
+              className="no-underline hover:underline ... text-blue-600"
+              href="https://www.redditinc.com/policies/user-agreement"
+            >
+              User Agreement
+            </a>{" "}
+            and{" "}
+            <a
+              className="no-underline hover:underline ... text-blue-600"
+              href="https://www.redditinc.com/policies/privacy-policy"
+            >
+              Privacy Policy.
+            </a>
+          </p>
+          <form>
+            <div className="mb-6">
+              <input
+                type="checkbox"
+                className="mr-1 cursor-pointer"
+                id="agreement"
+              />
+              <label htmlFor="agreement" className="text-xs cursor-pointer">
+                I agree to get emails about cool stuff on Maddit
+              </label>
+            </div>
+            <div className="mb-2">
+              <input
+                type="email"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="text"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="password"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Password"
+              />
+            </div>
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+              Sign Up
+            </button>
+          </form>
+          <small>
+            Already a madditor?
+            <Link href="/login">
+              <a className="ml-1 text-blue-500 uppercase">Login</a>
+            </Link>
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
